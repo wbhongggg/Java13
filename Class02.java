1748abd6ef312dcb1ac701619b165e2fb2632056
wbhongggg <92263847+wbhongggg@users.noreply.github.com>
2021-12-06 11:58:16 +0800

    Add files via upload

---
+++ Class02.java
@@ -0,0 +1,17 @@
+package Java13;
+public class Class02
+{
+	public static void main(String[] args)
+	{
+		int num=12,den=0;
+		int ans=num/den;
+		if(den==0)
+		{
+			System.out.println("°£¼Æ¬°0");
+		}
+		else
+		{
+			System.out.println("ans="+ans);
+		}
+	}
+}
---
+++ Class10.java
@@ -0,0 +1,9 @@
+package Java13;
+
+public class Class10
+{
+	public static void main(String[] args)
+	{
+		
+	}
+}
